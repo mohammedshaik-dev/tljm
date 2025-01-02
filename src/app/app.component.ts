a91b1870d64fe9d686cb223af69abe7217230bff
Abbas <abbassha28@gmail.com>
2025-01-03 00:37:42 +0530

    Added a template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent,FooterComponent,RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  template: `
+  <app-header></app-header>
+  <router-outlet></router-outlet>
+  <app-footer></app-footer>
+`,
 })
 export class AppComponent {
   title = 'triplicane-masjid-website';
