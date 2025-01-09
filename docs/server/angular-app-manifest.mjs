0886a80fe76afc49efb8b440e86b06fc7f95cb60
Abbas <abbassha28@gmail.com>
2025-01-09 14:19:13 +0530

    Publishing to Githubv1.5

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -6,8 +6,8 @@ export default {
   locale: undefined,
   routes: undefined,
   assets: {
-    'index.csr.html': {size: 169928, hash: 'b14b77a7788cb5d520de9448bc7854218f5b673708197f43f73238a091f46040', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 162595, hash: 'f1e61dde1ac5e95510eb3757cf479c90c0c43379eb52c32eef98d881c7c7105e', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 169928, hash: 'deffd9144798a936ed62e2e1b40cd663b8b15624b57b1b9d05f8d9b3c45255f6', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 162595, hash: '9008daa97a3407c4e402ad95607c051813b73b951f32d1220d0b5a9fcd4eaed1', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-XQMBQOO6.css': {size: 405586, hash: 'ba51waO4+bE', text: () => import('./assets-chunks/styles-XQMBQOO6_css.mjs').then(m => m.default)}
   },
 };
