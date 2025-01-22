f7cf04d7f89ff3bcf14a32e1c78a1809f333a26c
Abbas <abbassha28@gmail.com>
2025-01-22 11:24:14 +0530

    Publishing to Githubv2.2

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -6,8 +6,8 @@ export default {
   locale: undefined,
   routes: undefined,
   assets: {
-    'index.csr.html': {size: 171445, hash: '89a21adcc813e98b10770eb3909a01de823e917ef5e37cf60ffeba4d0544f1ae', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 164112, hash: '8c0e3a74b43672a4a9a70b9a12ae8e6faecebebb03c7ae93b6af62f134878725', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-XQMBQOO6.css': {size: 405586, hash: 'ba51waO4+bE', text: () => import('./assets-chunks/styles-XQMBQOO6_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 172978, hash: 'e3bff0f85b8e08539d755997248af27ebd50d1689953eef1f63377c44c1b0241', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 165651, hash: 'ba248089d371112e800c5429ad5e1e12dd7628edbee91cb5037104b6b459d34d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-V5E5IOQ3.css': {size: 405581, hash: 'OQb5uSfAGKs', text: () => import('./assets-chunks/styles-V5E5IOQ3_css.mjs').then(m => m.default)}
   },
 };
